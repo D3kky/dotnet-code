4b41b104aba2a45986f1f0d8e06148fa526efc19
dekky-vm-home <imdejan00@gmail.com>
2026-09-24 16:55:29 +0200

    LOL

--- App/Utilities/Constants.cs
+++ App/Utilities/Constants.cs
@@ -10,6 +10,7 @@ public static class Constants
   public const string ArgNamePrefix = "--";
   public const string Path = "path";
 
+  // TESTUNG
   public const string DotnetCode = nameof(DotnetCode);
   public const string Workspaces = nameof(Workspaces);
   public const string WorkspaceExtension = ".code-workspace";
